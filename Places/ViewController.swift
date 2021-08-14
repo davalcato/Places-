0889a1996ee4beaf2dd438eaddab5182e52023fa
davalcato <davalcato@yahoo.com>
2021-08-14 15:37:51 -0700

    first commit

---
+++ Places/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by Daval Cato on 8/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
