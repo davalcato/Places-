d30ffb5305741bf5dd08d20d12fb8f68c3cc3cab
davalcato <davalcato@yahoo.com>
2021-08-15 12:41:08 -0700

    Added the secondarySystemBackground to the searchVC to show on top of the map-view. Then I UISearchResultsUpdating to the class ViewController to show the results in the searchBar.

---
+++ Places/ResultsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ResultsViewController.swift
+//  Places
+//
+//  Created by Daval Cato on 8/15/21.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Background color
+        view.backgroundColor = .systemBlue
+    }
+    
+
+}
--- Places/ViewController.swift
+++ Places/ViewController.swift
@@ -6,12 +6,45 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchResultsUpdating {
+    
+    
+    // Create a map
+    let mapView = MKMapView()
+    
+    // Adding a search controller
+    let searchVC = UISearchController(searchResultsController: ResultsViewController())
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Add subview
+        title = "Maps"
+        view.addSubview(mapView)
+        // SeaarchBar a background color
+        searchVC.searchBar.backgroundColor = .secondarySystemBackground
+        
+        // Conform to protocol
+        searchVC.searchResultsUpdater = self
+        
+        navigationItem.searchController = searchVC
+        
+    }
+    // Override
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        // Add frame
+        mapView.frame = CGRect(
+            x: 0,
+            y: view.safeAreaInsets.top,
+            width: view.frame.size.width,
+            height: view.frame.size.height - view.safeAreaInsets.top)
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        
     }
 
 
